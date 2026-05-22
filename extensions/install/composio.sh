61dff6adb5c67b83456089b6549f49007ab0f16c
tilleulenspiegel <thebit@rocketmail.com>
2026-05-22 19:40:36 +0200

    fix(extensions/composio): Browser-UA setzen gegen Cloudflare-Block

--- extensions/install/composio.sh
+++ extensions/install/composio.sh
@@ -41,6 +41,7 @@ headers = {
     "x-api-key": API_KEY,
     "Content-Type": "application/json",
     "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
 }
 
 def api(method, path, body=None):
